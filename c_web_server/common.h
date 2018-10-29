9a8c6637628bc6b182018c8c3fb0472566549989
lijie <jie.li@equotaenergy.com>
2018-10-29 22:09:36 +0800

    modify project's struct

---
+++ c_web_server/common.h
@@ -0,0 +1,21 @@
+//
+//  common.h
+//  c_web_server
+//
+//  Created by MacOS on 2018/10/29.
+//  Copyright © 2018年 MacOS. All rights reserved.
+//
+
+#ifndef common_h
+#define common_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#endif /* common_h */
